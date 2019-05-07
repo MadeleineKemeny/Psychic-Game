aa78e07f7983186e60904b0881d17b9161753c1c
Madeleine Kemeny <madeleinekemeny@gmail.com>
2019-05-07 17:19:21 -0500

    Initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,51 @@
+//how to create randomized "computer" string choices that are stored as console log?//
+
+var letters = ["string"];
+    function allLetter(inputtxt)
+
+
+//onkey event; distinguish between "correct/incorrect guesses/remaining guesses"?//
+var letters = [ //array for entire alphabet--abbreviation?//
+    ];
+    if ("inputtxt is letters"); true //loop to continue with remaining guesses//
+      ;
+    if  ("input isn't the correct guess"); false, //change incorrect guesses ++ and remaining guesses--;loop to continue with remaining guesses//")
+    else //something other than a letter key is pressed//
+     {
+     alert("Please touch a letter-key only.");
+     return //loop to continue same guess;
+     }
+  }
+  
+
+
+// #s: scoring and guesses... pretty sure i'm missing things?//
+
+var score = 0;
+if (userInput === letters) {
+    alert("Correct!");
+    score.innerHTML = "Correct Guesses" ++;
+    updateScore();
+  }
+
+else {
+    alert("Incorrect");
+    score.innerHTML = "Incorrect guesses" ++;
+    updateScore(); //distinguish between "correct/incorrect guesses"; loop for new onkey event?//
+  }
+
+function updateScore() {
+    document.querySelector("#score").innerHTML = "Correct guesses " + score;
+
+// new game? //
+var score; {
+if (score === 10, guesses ===10,) {
+    alert ("GAME OVER")
+    }
+    
+    
+alert("Play again? Y/N");
+//onkey event//
+if (user userInput === "Y") //loop to start game from beginning//  
+if (userInput === "N") {
+} //end//
