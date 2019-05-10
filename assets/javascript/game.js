12dd4862c89886056843099cb23f4d75be1c9777
Madeleine Kemeny <madeleinekemeny@gmail.com>
2019-05-09 22:56:16 -0500

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,13 +5,12 @@ var wins = 0
 var losses = 0
 var remainingGuesses = 10
 var lettersTried = ""
-var results = alphabet.join();
 
 // defining array from which computer can choose:
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var x = document.getElementById ("lettersTried");
-//===> HOW DO I ENTER A RULE THAT DOESN'T ALLOW A KEY THAT ISN'T A LETTER; OR REPEATED GUESSES? CURRENTLY COUNTING ALL ONKEY EVENTS...
-// "if eventkey === integer or non-character key, FALSE; alert: "That's not a letter. Please pick a letter."; do not display in lettersTried or subtract from guessesRemaining.
+var x = document.getElementById("lettersTried");
+//===> HOW DO I ENTER A RULE THAT DOESN'T ALLOW OR COUNT REPEATED GUESSES?
+
 
 
 
@@ -20,33 +19,52 @@ var x = document.getElementById ("lettersTried");
 // see conditional comments below...
 document.onkeydown = function (event) {
   console.log("myletter:", event.key)
-  var letterClick = event.key.toLowerCase()
-
-  if (computerLetter === letterClick){
-    wins++
-    document.querySelector("#wins").innerHTML = wins
-    restart()
+ var keypressed= event.key.toLowerCase();
+  console.log("lettersTried" +  lettersTried.indexOf(keypressed) );
+  //if keyCode is outside of 65-90, ignore (compare "player letter" with the computerletter; if guess does not match index, then ALERT and do not update "# of remaining guesses --")
+  if (event.keyCode > 90 || event.keyCode < 65) {
+    alert("That's not a letter. Please pick a letter.")
   }
-  
+  else if (lettersTried.indexOf(keypressed) > -1) {
+    alert("You've tried this letter. Please pick a different letter.");
+  }
   else {
-    remainingGuesses--
-    lettersTried = lettersTried + letterClick  
-      document.querySelector("#lettersTried").innerHTML = lettersTried
-      document.querySelector("#remainingGuesses").innerHTML = remainingGuesses 
-  } 
+    //if between 65-90, proceeds to next IF/ELSE, line 32/40 
 
-  if (remainingGuesses === 0) {
-    losses++
-    document.querySelector("#losses").innerHTML = losses
-    restart()
+    var letterClick = event.key.toLowerCase();
+
+    // compare "player letter" with the computerletter; if guess matches computerletter, update "wins++""; restart program.
+    if (computerLetter === letterClick) {
+      wins++
+      document.querySelector("#wins").innerHTML = " " + wins
+      restart()
+    }
+
+    // compare "player letter" with the computerletter; if guess does not match computerletter, then ELSE: update "letters tried" display with guesses entered by player and "# of remaining guesses --"
+    else {
+      remainingGuesses--;
+      if (lettersTried !== "") {
+        lettersTried = lettersTried + " , " + letterClick;
+      }
+      else {
+        lettersTried = " " + lettersTried + letterClick;
+      }
+
+
+      document.querySelector("#lettersTried").innerHTML = lettersTried
+      document.querySelector("#remainingGuesses").innerHTML = " " + remainingGuesses
+    }
+
+    // if "remaining guesses" > 0, keep running program; "remaining guesses === 0", update "losses++"; restart game.
+    if (remainingGuesses === 0) {
+      losses++
+      document.querySelector("#losses").innerHTML = " " + losses
+      restart()
+    }
+
+
   }
 }
-
-// 1st IF (TRUE): compare "player letter" with the computerletter; if guess matches computerletter, update "wins++""; restart program.
-
-// 1st IF (FALSE): compare "player letter" with the computerletter; if guess does not match computerletter, then ELSE: update "letters tried" display with guesses entered by player and "# of remaining guesses --"
-// if "remaining guesses" > 0, keep running program.
-// 2ND IF "remaining guesses === 0", update "losses++"; restart game.
 
 // creates random string choices that are stored as console log
 function calculateComputer() {
@@ -58,9 +76,9 @@ function calculateComputer() {
 // where to populate results:
 function start() {
   console.log("start")
-  document.querySelector("#wins").innerHTML = "0"   //clear all the variables
-  document.querySelector("#losses").innerHTML = "0"   //clear all the variables
-  document.querySelector("#remainingGuesses").innerHTML = "10" 
+  document.querySelector("#wins").innerHTML = " 0"   //clear all the variables
+  document.querySelector("#losses").innerHTML = " 0"   //clear all the variables
+  document.querySelector("#remainingGuesses").innerHTML = " 10"
   calculateComputer()
 }
 
